2e909efdb2c1823ff3264d80ac759be4f696a8dd
elijahgott <gottelijah@gmail.com>
2024-01-12 14:41:52 -0600

    changed "hate" to "dislike"

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Navbar expand = "lg" className="bg-body-tertiary" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">LeBron James Hate Club</Navbar.Brand>
+            <Navbar.Brand href="#home">LeBron James Dislike Club</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
