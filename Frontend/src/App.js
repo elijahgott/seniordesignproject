a945d2c9d52dea81581653ec7f0dc2dd45410116
elijahgott <gottelijah@gmail.com>
2024-01-24 14:21:59 -0600

    start getting rid of LeBum

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <div className="App">
       <Navbar expand = "lg" className="bg-body-tertiary" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">LeBron James Dislike Club</Navbar.Brand>
+            <Navbar.Brand href="#home" style={{fontWeight: 'bold'}}>Music App</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
